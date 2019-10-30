db92e70b51bea362db5d715dcae12c9fefddee6c
Clayton Jones <claytonjonesLMP@gmail.com>
2019-10-29 20:47:11 -0700

    commented out console logs

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ alert('Here is a little quiz to help you get to know me. Please answer yes/no or
 var userAnswerAge = prompt('Am I 27 years old?').toLowerCase();
 console.log(userAnswerAge);
 if (userAnswerAge === 'yes' || userAnswerAge === 'y') {
-  console.log('Correct!');
+  // console.log('Correct!');
   alert('Correct!');
 } else {
-  console.log('Incorrect!');
+  // console.log('Incorrect! I am 27 years old.');
   alert('Incorrect! I am 27 years old.');
 }
 
@@ -18,10 +18,10 @@ var userAnswerHobbies = prompt('Is one of my hobbies origami?').toLowerCase();
 console.log(userAnswerHobbies);
 
 if (userAnswerHobbies === 'no' || userAnswerHobbies === 'n') {
-  console.log('Correct!');
+  // console.log('Correct!');
   alert('Correct!');
 } else {
-  console.log('Incorrect!');
+  // console.log('Incorrect! Origami is not one of my hobbies.');
   alert('Incorrect! Origami is not one of my hobbies.');
 }
 
@@ -31,27 +31,27 @@ if (userAnswerCollege === 'yes' || userAnswerCollege === 'y') {
   console.log('Correct!');
   alert('Correct!');
 } else {
-  console.log('Incorrect.');
+  // console.log('Incorrect. I did attend the UW.');
   alert('Incorrect! I did attend the UW.');
 }
 
 var userAnswerJob = prompt('Am I currently working as a supply chain manager at Amazon?').toLowerCase();
 console.log(userAnswerJob);
 if (userAnswerJob === 'yes' || userAnswerJob === 'y') {
-  console.log('Incorrect!');
+  // console.log('Incorrect! I am currently working as a massage therapist.');
   alert('Incorrect! I am currently working as a massage therapist.');
 } else {
-  console.log('Correct.');
+  // console.log('Correct! I am a massage therapist.');
   alert('Correct! I am a massage therapist.');
 }
 
 var userAnswerFuture = prompt('Am I currently attending Code Fellows to learn to be a JavaScript developer in the future?').toLowerCase();
 console.log(userAnswerFuture);
 if (userAnswerFuture === 'yes' || userAnswerFuture === 'y') {
-  console.log('Correct!');
+  // console.log('Correct!');
   alert('Correct!');
 } else {
-  console.log('Incorrect.');
+  // console.log('Incorrect. It is true!');
   alert('Incorrect! It is true!');
 }
 
