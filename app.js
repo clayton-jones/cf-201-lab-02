fec801c0dd970c5996b5cec866cf10dcc9beeb79
Clayton Jones <claytonjonesLMP@gmail.com>
2019-10-31 19:43:13 -0700

    Added 6th question asking user to guess number

--- app.js
+++ app.js
@@ -67,6 +67,31 @@ if (userAnswerFuture === 'yes' || userAnswerFuture === 'y') {
   alert('Incorrect! It is true!');
 }
 
+// creates random number between 1 and 100 for user to guess
+var myNumber = Math.floor((Math.random() * 100) + 1);
+console.log(myNumber);
+
+// asks user to guess number (first guess)
+var userGuess = parseInt(prompt('I am thinking of a number between 1 and 100. What is it? You have four guesses.'));
+
+
+// tells user if they got it right, and if not, to keep guessing
+for (var i = 0; i < 4; i++) {
+  console.log(userGuess);
+  if (i === 3) {
+    alert('Good try! The number was ' + myNumber);
+  } else if (userGuess === myNumber) {
+    alert('You got it! Amazing!');
+    i = 5;
+  } else if (userGuess < myNumber) {
+    alert('Too low!');
+    userGuess = parseInt(prompt('Guess higher! Your last guess was ' + userGuess + '. ' + (3 - i) + ' guesses left.'));
+  } else {
+    alert('Too high!');
+    userGuess = parseInt(prompt('Guess lower! Your last guess was ' + userGuess + '. ' + (3 - i) + ' guesses left.'));
+  }
+}
+
 
 // Farewell message
 alert('Thank you for taking the time to learn a little bit about me, ' + userName + '! Your final score is ' + score + 'out of 7.');
