48ad2c244ac1d6c3ba00d6aebdd9ae35f9ce8333
CAS OLEJNICZAK <CASOLEJNICZAK@GMAIL.COM>
2019-11-02 13:42:57 -0700

    moved alert into function in seventh question

--- app.js
+++ app.js
@@ -2,124 +2,124 @@
 
 // Asking user's name
 var userName = prompt('What is your name?');
-alert('Hi, ' + userName + ', welcome to my awesome website. Impressed?');
-alert('Here is a little quiz to help you get to know me. Please answer yes/no or y/n.');
+// alert('Hi, ' + userName + ', welcome to my awesome website. Impressed?');
+// alert('Here is a little quiz to help you get to know me. Please answer yes/no or y/n.');
 
 var score = 0;
 
-// First question
+// // First question
 
-function firstQuestion() {
-  var userAnswerAge = prompt('Am I 27 years old?').toLowerCase();
-  // console.log(userAnswerAge);
-  if (userAnswerAge === 'yes' || userAnswerAge === 'y') {
-    // console.log('Correct!');
-    alert('Correct!');
-    score++;
-  } else {
-    // console.log('Incorrect! I am 27 years old.');
-    alert('Incorrect! I am 27 years old.');
-  }
-}
-firstQuestion();
+// function firstQuestion() {
+//   var userAnswerAge = prompt('Am I 27 years old?').toLowerCase();
+//   // console.log(userAnswerAge);
+//   if (userAnswerAge === 'yes' || userAnswerAge === 'y') {
+//     // console.log('Correct!');
+//     alert('Correct!');
+//     score++;
+//   } else {
+//     // console.log('Incorrect! I am 27 years old.');
+//     alert('Incorrect! I am 27 years old.');
+//   }
+// }
+// firstQuestion();
 
 
-//  Second question
+// //  Second question
 
-function secondQuestion() {
-  var userAnswerHobbies = prompt('Is one of my hobbies origami?').toLowerCase();
-  // console.log(userAnswerHobbies);
+// function secondQuestion() {
+//   var userAnswerHobbies = prompt('Is one of my hobbies origami?').toLowerCase();
+//   // console.log(userAnswerHobbies);
   
-  if (userAnswerHobbies === 'no' || userAnswerHobbies === 'n') {
-    // console.log('Correct!');
-    alert('Correct!');
-    score++;
-  } else {
-    // console.log('Incorrect! Origami is not one of my hobbies.');
-    alert('Incorrect! Origami is not one of my hobbies.');
-  }
-}
-secondQuestion();
+//   if (userAnswerHobbies === 'no' || userAnswerHobbies === 'n') {
+//     // console.log('Correct!');
+//     alert('Correct!');
+//     score++;
+//   } else {
+//     // console.log('Incorrect! Origami is not one of my hobbies.');
+//     alert('Incorrect! Origami is not one of my hobbies.');
+//   }
+// }
+// secondQuestion();
 
 
-// Third question
+// // Third question
 
-function thirdQuestion() {
-  var userAnswerCollege = prompt('Did I attend college at the University of Washington?').toLowerCase();
-  // console.log(userAnswerCollege);
-  if (userAnswerCollege === 'yes' || userAnswerCollege === 'y') {
-    // console.log('Correct!');
-    alert('Correct!');
-    score++;
-  } else {
-    // console.log('Incorrect. I did attend the UW.');
-    alert('Incorrect! I did attend the UW.');
-  }
-}
-thirdQuestion();
+// function thirdQuestion() {
+//   var userAnswerCollege = prompt('Did I attend college at the University of Washington?').toLowerCase();
+//   // console.log(userAnswerCollege);
+//   if (userAnswerCollege === 'yes' || userAnswerCollege === 'y') {
+//     // console.log('Correct!');
+//     alert('Correct!');
+//     score++;
+//   } else {
+//     // console.log('Incorrect. I did attend the UW.');
+//     alert('Incorrect! I did attend the UW.');
+//   }
+// }
+// thirdQuestion();
 
-// Fourth question
+// // Fourth question
 
-function fourthQuestion() {
-  var userAnswerJob = prompt('Am I currently working as a supply chain manager at Amazon?').toLowerCase();
-  // console.log(userAnswerJob);
-  if (userAnswerJob === 'yes' || userAnswerJob === 'y') {
-    // console.log('Incorrect! I am currently working as a massage therapist.');
-    alert('Incorrect! I am currently working as a massage therapist.');
-  } else {
-    // console.log('Correct! I am a massage therapist.');
-    alert('Correct! I am a massage therapist.');
-    score++;
-  }
-}
-fourthQuestion();
+// function fourthQuestion() {
+//   var userAnswerJob = prompt('Am I currently working as a supply chain manager at Amazon?').toLowerCase();
+//   // console.log(userAnswerJob);
+//   if (userAnswerJob === 'yes' || userAnswerJob === 'y') {
+//     // console.log('Incorrect! I am currently working as a massage therapist.');
+//     alert('Incorrect! I am currently working as a massage therapist.');
+//   } else {
+//     // console.log('Correct! I am a massage therapist.');
+//     alert('Correct! I am a massage therapist.');
+//     score++;
+//   }
+// }
+// fourthQuestion();
 
-// Fifth question
+// // Fifth question
 
-function fifthQuestion() {
-  var userAnswerFuture = prompt('Am I currently attending Code Fellows to learn to be a JavaScript developer in the future?').toLowerCase();
-  // console.log(userAnswerFuture);
-  if (userAnswerFuture === 'yes' || userAnswerFuture === 'y') {
-    // console.log('Correct!');
-    alert('Correct!');
-    score++;
-  } else {
-    // console.log('Incorrect. It is true!');
-    alert('Incorrect! It is true!');
-  }
-}
-fifthQuestion();
+// function fifthQuestion() {
+//   var userAnswerFuture = prompt('Am I currently attending Code Fellows to learn to be a JavaScript developer in the future?').toLowerCase();
+//   // console.log(userAnswerFuture);
+//   if (userAnswerFuture === 'yes' || userAnswerFuture === 'y') {
+//     // console.log('Correct!');
+//     alert('Correct!');
+//     score++;
+//   } else {
+//     // console.log('Incorrect. It is true!');
+//     alert('Incorrect! It is true!');
+//   }
+// }
+// fifthQuestion();
 
-// Sixth question
+// // Sixth question
 
-function sixthQuestion() {
-  // creates random number between 1 and 100 for user to guess
-  var myNumber = Math.floor((Math.random() * 100) + 1);
-  // console.log(myNumber); // used to quickly test correct answers
+// function sixthQuestion() {
+//   // creates random number between 1 and 100 for user to guess
+//   var myNumber = Math.floor((Math.random() * 100) + 1);
+//   // console.log(myNumber); // used to quickly test correct answers
   
-  // asks user to guess number (first guess)
-  var userGuess = parseInt(prompt('I am thinking of a number between 1 and 100. What is it? You have four guesses.'));
-  // console.log('userGuess number 1 to 100: ', userGuess);
+//   // asks user to guess number (first guess)
+//   var userGuess = parseInt(prompt('I am thinking of a number between 1 and 100. What is it? You have four guesses.'));
+//   // console.log('userGuess number 1 to 100: ', userGuess);
   
-  // tells user if they got it right, and if not, to keep guessing
-  for (var i = 0; i < 4; i++) {
-    // console.log(userGuess);
-    if (userGuess === myNumber) {
-      alert('You got it! Amazing!');
-      score++;
-      break;
-    } else if (i === 3) {
-      alert('Good try! The number was ' + myNumber);
-    } else if (userGuess < myNumber) {
-      alert('Too low!');
-      userGuess = parseInt(prompt('Guess higher! Your last guess was ' + userGuess + '. ' + (3 - i) + ' guesses left.'));
-    } else {
-      alert('Too high!');
-      userGuess = parseInt(prompt('Guess lower! Your last guess was ' + userGuess + '. ' + (3 - i) + ' guesses left.'));
-    }
-  }
-}
-sixthQuestion();
+//   // tells user if they got it right, and if not, to keep guessing
+//   for (var i = 0; i < 4; i++) {
+//     // console.log(userGuess);
+//     if (userGuess === myNumber) {
+//       alert('You got it! Amazing!');
+//       score++;
+//       break;
+//     } else if (i === 3) {
+//       alert('Good try! The number was ' + myNumber);
+//     } else if (userGuess < myNumber) {
+//       alert('Too low!');
+//       userGuess = parseInt(prompt('Guess higher! Your last guess was ' + userGuess + '. ' + (3 - i) + ' guesses left.'));
+//     } else {
+//       alert('Too high!');
+//       userGuess = parseInt(prompt('Guess lower! Your last guess was ' + userGuess + '. ' + (3 - i) + ' guesses left.'));
+//     }
+//   }
+// }
+// sixthQuestion();
 
 //Seventh Question
 
@@ -149,6 +149,7 @@ function seventhQuestion() {
       userVideogameGuess = prompt('Guess again! ' + (6 - numOfGuesses) + ' remaining.').toLowerCase();
     }
   }
+  alert('These were all of the possible answers: ' + myFavVideogames);
 }
 seventhQuestion();
 
@@ -157,7 +158,6 @@ seventhQuestion();
 //   numOfGuesses++;
 // }
 
-alert('These were all of the possible answers: ' + myFavVideogames);
 
 
 // Farewell message
